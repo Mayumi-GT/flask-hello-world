3dd4196a64f93297f04dc03a3aa861d60338802a
Mayumi S <mayumi.marbury@gmail.com>
2023-03-21 21:48:30 +0000

    /db_create route created

--- app.py
+++ app.py
@@ -12,3 +12,20 @@ def testing():
   conn = psycopg2.connect("postgres://mash8545_render_user:OAikQEaPWJ0YlxAEgBV5x7wvyd4BoAHQ@dpg-cgd20l7dvk4htnrc2hi0-a/mash8545_render")
   conn.close()
   return "Database Connection Successful"
+
+@app.route('/db_create')
+def creating():
+  conn = psycopg2.connect("postgres://mash8545_render_user:OAikQEaPWJ0YlxAEgBV5x7wvyd4BoAHQ@dpg-cgd20l7dvk4htnrc2hi0-a/mash8545_render")
+  cur = conn.cursor()
+  cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+  ''')
+  conn.commit()
+  conn.close()
+  return "Basketball Table Successfully Created"
