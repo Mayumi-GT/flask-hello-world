75295341840c391e9c0dfe6b34dcfa560c90c515
Mayumi S <mayumi.marbury@gmail.com>
2023-03-21 22:24:04 +0000

    /db_select and /db_drop draft created

--- app.py
+++ app.py
@@ -45,3 +45,39 @@ def inserting():
   conn.commit()
   conn.close()
   return "Basketball Table Successfully populated"
+
+@app.route('/db_select')
+def selecting():
+  try:
+    conn = psycopg2.connect("postgres://mash8545_render_user:OAikQEaPWJ0YlxAEgBV5x7wvyd4BoAHQ@dpg-cgd20l7dvk4htnrc2hi0-a/mash8545_render")
+    cur = conn.cursor()
+    cur.execute('''
+        SELECT * FROM Basketball;
+        ''')
+    records = cur.fetchall()
+    conn.close()
+    response_string=""
+    response_string+="<table>"
+    for player in records:
+      response_string+="<tr>"
+      for info in player:
+        response_string+="<td>{}</td>".format(info)
+      response_string+="</tr>"
+    response_string+="</table>"
+    return response_string
+  except Exception as e:
+    return "Error selecting data: " + str(e)
+
+@app.route('/db_drop')
+def dropping():
+  try:
+    conn = psycopg2.connect("postgres://mash8545_render_user:OAikQEaPWJ0YlxAEgBV5x7wvyd4BoAHQ@dpg-cgd20l7dvk4htnrc2hi0-a/mash8545_render")
+    cur = conn.cursor()
+    cur.execute('''
+        DROP TABLE Basketball;
+        ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Dropped"
+  except Exception as e:
+    return "Error dropping table: "
